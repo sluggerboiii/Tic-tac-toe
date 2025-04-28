f6fc5f83d77d3771419143072e3913e21f2a07e7
sluggerboiii <perfectcsengineer24@gmail.com>
2025-04-28 22:09:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); // Update URL after backend deployment
+const socket = io('http://34.93.246.160:4000');
 
 function App() {
   const [roomId, setRoomId] = useState('');
